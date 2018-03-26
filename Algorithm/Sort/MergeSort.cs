10f468fe181cb8d7ee0f579a76273f8e335e1307
任志伟 <renzhiwei@gridsum.com>
2018-03-26 16:52:47 +0800

    Add merge sort and its test

---
+++ Algorithm/Sort/MergeSort.cs
@@ -0,0 +1,48 @@
+﻿namespace Algorithm.Sort
+{
+    public class MergeSort : SortBase
+    {
+        public override void Sort(int[] arr)
+        {
+            if (arr.Length < 2)
+            {
+                return;
+            }
+
+            var arrHeader = new int[arr.Length / 2];
+            var arrFooter = new int[arr.Length - arrHeader.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < arrHeader.Length)
+                {
+                    arrHeader[i] = arr[i];
+                }
+                else
+                {
+                    arrFooter[i - arrHeader.Length] = arr[i];
+                }
+            }
+            Sort(arrHeader);
+            Sort(arrFooter);
+            Merge(arr, arrHeader, arrFooter);
+        }
+
+        private void Merge(int[] arr, int[] arrHeader, int[] arrFooter)
+        {
+            for (int i = 0, j = 0; i + j < arr.Length;)
+            {
+                if (j == arrFooter.Length || (i < arrHeader.Length && arrHeader[i] < arrFooter[j]))
+                {
+                    arr[i + j] = arrHeader[i];
+                    i++;
+                }
+                else
+                {
+                    arr[i + j] = arrFooter[j];
+                    j++;
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmTests/Sort/MergeSortTest.cs
@@ -0,0 +1,12 @@
+﻿using Algorithm.Sort;
+
+namespace AlgorithmTests.Sort
+{
+    public class MergeSortTest : SortBaseTest
+    {
+        public MergeSortTest()
+        {
+            Sort = new MergeSort();
+        }
+    }
+}
