881ed361ab04f72145db8c7c4b0800695967d596
renzhiwei <renzhiwei85@163.com>
2018-03-31 20:39:04 +0800

    add bucket sort

---
+++ Algorithm/Sort/BucketSort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Sort
+{
+    public class BucketSort : SortBase
+    {
+        public override void Sort(int[] arr)
+        {
+            ;
+        }
+    }
+}
---
+++ AlgorithmTests/Sort/BucketSortTest.cs
@@ -0,0 +1,12 @@
+﻿using Algorithm.Sort;
+
+namespace AlgorithmTests.Sort
+{
+    public class BucketSortTest : SortBaseTest
+    {
+        public BucketSortTest()
+        {
+            Sort = new BucketSort();
+        }
+    }
+}
