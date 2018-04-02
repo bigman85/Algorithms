0d5ac3f434e7482b7798c58046ad1b2b8ee32b34
任志伟 <renzhiwei@gridsum.com>
2018-04-02 11:43:56 +0800

    add rescursion binary search

--- Algorithm/Search/BinarySearch.cs
+++ Algorithm/Search/BinarySearch.cs
@@ -5,6 +5,20 @@
         public override int Search(int[] arr, int value)
         {
             int res = -1;
+            int min = 0;
+            int max = arr.Length - 1;
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (arr[mid] == value)
+                    return mid;
+                if (arr[mid] > value)
+                    max = mid - 1;
+                if (arr[mid] < value)
+                    min = mid + 1;
+            }
+
             return res;
         }
     }
